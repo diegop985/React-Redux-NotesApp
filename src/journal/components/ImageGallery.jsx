4112b4db0448142a800ec12d92abb358b9c865c3
Diego Ps <diegops985@gmail.com>
2023-08-25 21:32:01 -0500

    readme

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -3,7 +3,7 @@ import { ImageList, ImageListItem } from '@mui/material';
 
 export const ImageGallery = ( { images } ) => {
   return (
-    <ImageList sx={ { width: '100%', height: 500 } } cols={ 3 } rowHeight={ 200 }>
+    <ImageList sx={ { width: '100%', height: 500, display: 'flex', gap: 4 } } rowHeight={ 200 }>
       { images.map( ( image ) => (
         <ImageListItem key={ image }>
           <img
